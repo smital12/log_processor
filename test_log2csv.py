52a09613b4e0bc1b69ef3e87753083474946f9eb
Alex Smith Torres <smital12@hotmail.com>
2018-08-23 21:37:51 -0700

    fleshed out LSB test and test for adc file

--- test_log2csv.py
+++ test_log2csv.py
@@ -2,24 +2,44 @@ import log2csv
 import py.test
 
 
+test = 'testname'
+
+
 def test_remove_LSBs():
-    test = 'testname'
-    lines = [test + '\t' + x + '\t' for x in ['LSB', 'mLSB', 'KLSB']]
-    lines_clean = [test + '\t' + x for x in ['', 'm', 'K']]
-    assert log2csv.remove_LSBs(lines, test) == lines_clean
-# def striplist(l):
-# def remove_celcius_C(match):
-# def convert_SI_prefix_to_E_notation(match):
-# def remove_LSBs(inLines, testName, isADCTest=False,
-# def remove_SI_prefixes(inLines, testName, isADCTest=False,
-# def tab_delimit_test_lines(inLines, testName, isADCTest=False,
-# def format_header(oldHeader):
-# def format_all_headers(inLines, isHeaderEveryTime=False):
-# def get_test_lines(inLines, testName, isADCTest=False,
-# def get_header_and_test_lines(inLines, testName, isADCTest=False,
-# def move_MCs_list_to_test_line(inLines):
-# def move_header_into_lines(inLines, isADCTest=True, isHeaderEveryTime=False):
-# def get_header(isADCTest, isHeaderEveryTime):
-# def get_full_header(isADCTest):
-# def get_parametric_header(isADCTest):
-# def main(filename, testname, isADCTest, isHeaderEveryTime):
+    lineIn = [test + '\tLSB\t']
+    lineOut = [test + '\t']
+    assert log2csv.remove_LSBs(lineIn, test) == lineOut
+
+
+def test_remove_LSBs_with_prefix():
+    prefixes = ['M',
+                'K',
+                'm',
+                'u',
+                'n',
+                'p',
+                'f']
+    lines1 = [test + '\t' + x + 'LSB\t' for x in prefixes]
+    lines2 = [test + '\t' + x + '\t' for x in prefixes]
+    assert log2csv.remove_LSBs(lines1, test) == lines2
+
+
+def test_get_test_lines():
+    inLines = ['line0\t' + test,
+               'line1\t',
+               'line2\t' + test,
+               'line4\t',
+               'line5\t' + test]
+    outLines = ['line0\t' + test,
+                'line2\t' + test,
+                'line5\t' + test]
+    assert log2csv.get_test_lines(inLines, test) == outLines
+
+def test_log2csv_adc():
+    inFile = './test/test_logs_01/test_adc.txt'
+    outFile = inFile + '_cleaned'
+    referenceFile = inFile + '_cleaned.reference'
+
+    log2csv.main(inFile, 'adclin', isADCTest=True, isHeaderEveryTime=True)
+
+    assert open(outFile, 'r').read() == open(referenceFile , 'r').read()
